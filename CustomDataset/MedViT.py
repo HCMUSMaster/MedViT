9e73a76d3aff16b922ccf3adf7bb18a36a690f3e
Kafka <kafka@undercurrent.tech>
2026-03-26 16:32:39 +0000

    fix __init__

--- CustomDataset/MedViT.py
+++ CustomDataset/MedViT.py
@@ -412,7 +412,7 @@ class LTB(nn.Module):
 class MedViT(nn.Module):
     def __init__(self, stem_chs, depths, path_dropout, attn_drop=0, drop=0, num_classes=1000,
                  strides=[1, 2, 2, 2], sr_ratios=[8, 4, 2, 1], head_dim=32, mix_block_ratio=0.75,
-                 use_checkpoint=False):
+                 use_checkpoint=False, **kwargs):
         super(MedViT, self).__init__()
         self.use_checkpoint = use_checkpoint
 
